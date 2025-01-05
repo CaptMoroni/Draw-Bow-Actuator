51ec51d78a3ae457c22c6f94f933e86ce9fdaad2
CaptMoroni <stevetsorensen@gmail.com>
2025-01-04 22:31:56 -0500

    working on control_loadcell for calibration routine

--- Software Package V2.0/control_loadcell.py
+++ Software Package V2.0/control_loadcell.py
@@ -1,4 +1,5 @@
 import control_settings as cs
+from matrix import *
 
 class loadCellTools:
     def __init__(self):
@@ -19,3 +20,13 @@ class loadCellTools:
     def convertRawLoad(self, x):
         lineFitLoad =  self.a * (x**3) + self.b * (x**2) + self.c * x + self.d
         return lineFitLoad
+    
+    def setLineFitValues(self, d=0, c=0, b=0, a=0):
+        self.a = a
+        self.b = b
+        self.c = c 
+        self.d = d 
+        return
+    
+    def solveCurveFit(self, r1, a1, r2, a2):
+        return
